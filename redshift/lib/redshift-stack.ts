ba5d8274392f9c1273a5e436e8a297f18fd8bd73
Bruno Higino de Souza <brunohigino06@gmail.com>
2023-04-05 15:02:09 -0300

    Update redshift-stack.ts

--- redshift/lib/redshift-stack.ts
+++ redshift/lib/redshift-stack.ts
@@ -1,15 +1,15 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { Duration, Fn, Stack, StackProps } from 'aws-cdk-lib';
 import { aws_redshift as redshift } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import cryptoRandomString from 'crypto-random-string';
-
+import { aws_secretsmanager as secretsmanager } from 'aws-cdk-lib';
 
 export class RedshiftStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
     const mapfre_redshift_pipefy = new redshift.CfnCluster(this, 'mapfre_redshift_pipefy', {
-      clusterType: 'multi-node',
-      numberOfNodes: 2,
+      clusterType: 'single-node',
       dbName: 'mapfre_redshift_pipefy',
       masterUsername: 'administrator',
       masterUserPassword: cryptoRandomString({length: 10}),
@@ -20,7 +20,7 @@ export class RedshiftStack extends Stack {
       clusterType: 'single-node',
       dbName: 'mapfre_redshift_pipefy',
       masterUsername: 'administrator',
-      masterUserPassword: 'cryptoRandomString({length: 10})',
+      masterUserPassword: cryptoRandomString({length: 10}),
       nodeType: 'dc2.large',
     });
   }
