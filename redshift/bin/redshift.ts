1534f3aa6f66a4fd9600b7891225e852d46bccaf
Bruno Higino de Souza <brunohigino06@gmail.com>
2023-04-05 15:12:03 -0300

    05.04.2023

--- redshift/bin/redshift.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { RedshiftStack } from '../lib/redshift-stack';
-
-const app = new cdk.App();
-new RedshiftStack(app, 'RedshiftStack');
