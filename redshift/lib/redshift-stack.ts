2daa53d5d89954878b177d51e40cfd3d4425f1cf
Bruno Higino de Souza <brunohigino06@gmail.com>
2023-04-05 10:32:05 -0300

    Update redshift-stack.ts

--- redshift/lib/redshift-stack.ts
+++ redshift/lib/redshift-stack.ts
@@ -6,7 +6,7 @@ export class RedshiftStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
     const mapfre_redshift_pipefy = new redshift.CfnCluster(this, 'mapfre_redshift_pipefy', {
-      clusterType: 'single-node',
+      clusterType: '2',
       dbName: 'mapfre_redshift_pipefy',
       masterUsername: 'admin',
       masterUserPassword: '!QAZ2wsx',
