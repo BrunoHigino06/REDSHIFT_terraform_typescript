e9522a32ee83acfc38171a044abf5cff17ee482c
Bruno Higino de Souza <brunohigino06@gmail.com>
2023-04-05 09:57:12 -0300

    Update redshift-stack.ts

--- redshift/lib/redshift-stack.ts
+++ redshift/lib/redshift-stack.ts
@@ -5,14 +5,22 @@ import { Construct } from 'constructs';
 export class RedshiftStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-    const cfnCluster = new redshift.CfnCluster(this, 'mapfre_redshift_pipefy', {
+    const mapfre_redshift_pipefy = new redshift.CfnCluster(this, 'mapfre_redshift_pipefy', {
       clusterType: 'single-node',
       dbName: 'mapfre_redshift_pipefy',
       masterUsername: 'admin',
       masterUserPassword: '!QAZ2wsx',
       nodeType: 'dc2.large',
     });
-    
+
+    const mapfre_redshift_pipefy_analytics = new redshift.CfnCluster(this, 'mapfre_redshift_pipefy_analytics', {
+      clusterType: 'single-node',
+      dbName: 'mapfre_redshift_pipefy',
+      masterUsername: 'admin',
+      masterUserPassword: '!QAZ2wsx',
+      nodeType: 'dc2.large',
+    });
+
 
   }
 }
