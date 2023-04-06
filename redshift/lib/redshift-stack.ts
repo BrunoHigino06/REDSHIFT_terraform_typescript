d2d4cceb5d7b441940a2872ec230d7dc75164d17
Bruno Higino de Souza <brunohigino06@gmail.com>
2023-04-06 12:35:34 -0300

    Update redshift-stack.ts

--- redshift/lib/redshift-stack.ts
+++ redshift/lib/redshift-stack.ts
@@ -1,8 +1,7 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import { aws_redshift as redshift } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as cdk from 'aws-cdk-lib';
-
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 
 export class RedshiftStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -10,21 +9,23 @@ export class RedshiftStack extends Stack {
     
     const PipefySubnetGroups = new redshift.CfnClusterSubnetGroup(this, 'PipefySubnetGroups', {
       description: 'subnets groups for the redshift cluster',
-      subnetIds: ['subnet-017464efb386ea393', 'subnet-0cc0fbd3b62a4305a'],
+      subnetIds: ['subnet-0489ec3d142a8a4a2', 'subnet-071ad72cecd712e1b'],
       tags: [{
         key: 'Name',
         value: 'pipefy-subnet-groups',
       }],
     });
 
-    new cdk.CfnOutput(this, 'PipefySubnetGroupsRef', {
-      value: PipefySubnetGroups.attrClusterSubnetGroupName,
-      description: 'The name of the PipefySubnetGroups',
-      exportName: 'PipefySubnetGroups',
-    });
+    const SubnetGroups = PipefySubnetGroups.attrClusterSubnetGroupName
 
     var randomstring = require("randomstring");
     var password = randomstring.generate()
+
+    const ResdshiftClusterPassword = new secretsmanager.CfnSecret(this, 'ResdshiftClusterPassword', {
+      description: 'Redshift Cluster Password',
+      name: 'ResdshiftClusterPassword',
+      secretString: password,
+    });
     
     const mapfre_redshift_pipefy = new redshift.CfnCluster(this, 'mapfre_redshift_pipefy', {
       clusterType: 'single-node',
@@ -32,7 +33,7 @@ export class RedshiftStack extends Stack {
       masterUsername: 'administrator',
       masterUserPassword: password,
       nodeType: 'dc2.large',
-      clusterSubnetGroupName: cdk.Fn.importValue('PipefySubnetGroups'),
+      clusterSubnetGroupName: SubnetGroups,
     });
 
     mapfre_redshift_pipefy.addDependency(PipefySubnetGroups);
@@ -43,7 +44,7 @@ export class RedshiftStack extends Stack {
       masterUsername: 'administrator',
       masterUserPassword: password,
       nodeType: 'dc2.large',
-      clusterSubnetGroupName: cdk.Fn.importValue('PipefySubnetGroups')
+      clusterSubnetGroupName: SubnetGroups
     });
 
     mapfre_redshift_pipefy_analytics.addDependency(PipefySubnetGroups);
